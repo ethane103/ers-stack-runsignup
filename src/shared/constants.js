0ffd062e1eacb6f9c88d36344d949732978019f5
CPDragone <chrispauldragone@gmail.com>
2022-10-31 20:00:32 -0400

    buttons to populate, reset db and fill map

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -2,6 +2,8 @@ module.exports = {
     channels: {
       GET_DATA: 'get_data',
       RESET_DB: 'reset_db',
-      FILL_MAP: 'fill_map'
+      FILL_MAP: 'fill_map',
+      SEND_SQL: 'send_sql',
+      RECIEVE_SQL: 'recieve_sql'
     },
   };
